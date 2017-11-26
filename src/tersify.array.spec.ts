c4a9f89b3cdaa064d98009a66755c276d1153bf9
unional <homawong@gmail.com>
2017-11-26 14:40:57 -0800

    test: add array test

---
+++ src/tersify.array.spec.ts
@@ -0,0 +1,19 @@
+import { test } from 'ava'
+
+import { tersify } from './index'
+
+test('empty array', t => {
+  t.is(tersify([]), '[]')
+})
+
+test('array with primitive', t => {
+  t.is(tersify([1, false, 'a']), `[1, false, 'a']`)
+})
+
+test('array with object', t => {
+  t.is(tersify([{ a: 1 }]), `[{ a: 1 }]`)
+})
+
+test('object with array', t => {
+  t.is(tersify({ path: [], expected: a => a > 0, actual: 0 }), `{ path: [], expected: a => a > 0, actual: 0 }`)
+})
