40c4f50fa9415c0834e428d5cb00dd2d6881140c
Homa Wong <homawong@gmail.com>
2018-01-09 19:34:28 -0800

    fix: handle error in object. (#8)
    Remove some double whitespace

--- src/tersifyObject.ts
+++ src/tersifyObject.ts
@@ -1,18 +1,22 @@
 import stringifyObject = require('stringify-object');
 
 import { tersifyFunction } from './tersifyFunction'
+import { tersifyError } from './tersifyError';
 
 export function tersifyObject(obj, option = { maxLength: Infinity }) {
   let str: string = stringifyObject(obj, {
     indent: '  ',
     inlineCharacterLimit: Infinity,
     transform: (obj, prop, originalResult) => {
-      if (typeof obj[prop] === 'function')
-        return tersifyFunction(obj[prop], { maxLength: Infinity })
+      const value = obj[prop]
+      if (typeof value === 'function')
+        return tersifyFunction(value, option)
+      if (value instanceof Error)
+        return tersifyError(value, option)
       return originalResult
     }
   })
-  str = str.replace(/\{/g, '{ ').replace(/\}/g, ' }').replace(/{\s{2}}/g, '{}')
+  str = str.replace(/\{/g, '{ ').replace(/\}/g, ' }').replace(/{\s{2}}/g, '{}').replace(/{\s{2}/g, '{ ').replace(/\s{2}}/g, ' }')
   if (str.length > option.maxLength) {
     str = str.slice(0, option.maxLength - 5) + '...' + str.slice(-2)
   }
