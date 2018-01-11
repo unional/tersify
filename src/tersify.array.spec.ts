287e635fa62b04205fd69329c380edd9afbc8805
unional <homawong@gmail.com>
2018-01-10 22:40:14 -0800

    fix: array trim
    Fix trimming issue with array

--- src/tersify.array.spec.ts
+++ src/tersify.array.spec.ts
@@ -23,3 +23,26 @@ test('use tersify method for each element in array', t => {
   const b = tersible({ b: 2 }, () => 'b2')
   t.is(tersify([a, b, { c: 3 }, undefined, null, 1, 'a']), `[a1, b2, { c: 3 }, undefined, null, 1, 'a']`)
 })
+
+test('long array will be trimmed', t => {
+  const a = { aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa: 1, bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb: 2, ccccccccccccccccccccccccc: 3, ddddddddddddddddddddddddddddddddd: 4 }
+  t.is(tersify([a, a]), `[{ aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa: 1, bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb: 2, ccccccccccccccccccccccccc: 3, ... ]`)
+})
+
+test('maxLength: Infinity will return whole array without trim', t => {
+  const a = { aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa: 1, bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb: 2, ccccccccccccccccccccccccc: 3, ddddddddddddddddddddddddddddddddd: 4 }
+  t.is(tersify([a, /*istanbul ignore next*/ function (x, y) {
+    console.log(1)
+    console.log(2)
+    console.log(3)
+    console.log(4)
+    console.log(5)
+    console.log(6)
+    console.log(7)
+    console.log(8)
+    console.log(9)
+    console.log(10)
+    x++
+    return y
+  }], { maxLength: Infinity }), `[{ aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa: 1, bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb: 2, ccccccccccccccccccccccccc: 3, ddddddddddddddddddddddddddddddddd: 4 }, function (x, y) { console.log(1); console.log(2); console.log(3); console.log(4); console.log(5); console.log(6); console.log(7); console.log(8); console.log(9); console.log(10); x++; return y; }]`)
+})
--- src/tersifyArray.ts
+++ src/tersifyArray.ts
@@ -1,6 +1,18 @@
 import { tersify } from './tersify'
 
 export function tersifyArray(entries: Array<any>, option) {
-  const tersedEntries = entries.map(entry => tersify(entry, option))
-  return `[${tersedEntries.join(', ')}]`
+  let length = 1
+  const tersifiedEntries: string[] = []
+  entries.forEach(entry => {
+    if (length > option.maxLength) return
+
+    const tersifiedEntry = tersify(entry, option)
+    length += tersifiedEntry.length
+    tersifiedEntries.push(tersifiedEntry)
+  })
+  const tersified = `[${tersifiedEntries.join(', ')}]`
+  if (tersified.length > option.maxLength) {
+    return tersified.slice(0, option.maxLength - 5) + '... ]'
+  }
+  return tersified
 }
