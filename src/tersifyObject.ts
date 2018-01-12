86c26af8ccb261bc6f75fd8a1d0481f312f41008
Homa Wong <homawong@gmail.com>
2018-01-11 23:04:09 -0800

    fix: remove stringify-object.
    Don't need it and will have more control

--- src/tersifyObject.ts
+++ src/tersifyObject.ts
@@ -1,22 +1,22 @@
-// NOTE: May not need stringify-object anymore, just iterate and repeat
-import stringifyObject = require('stringify-object')
-
 import { defaultTersify } from './defaultTersify'
 import { isTersible } from './tersible'
 import { tersify } from './tersify'
 
+function stringifyObject(obj, options) {
+  const nodes: any[] = []
+  Object.keys(obj).forEach(k => {
+    const value = obj[k]
+    if (value === undefined)
+      return
+    nodes.push(`${k}: ${tersify(value, options)}`)
+  })
+  return nodes.length === 0 ? '{}' : `{ ${nodes.join(', ')} }`
+}
+
 export function tersifyObject(obj, option) {
   let str: string = isTersible(obj) && obj['tersify'] !== defaultTersify ?
     obj.tersify(option) :
-    stringifyObject(obj, {
-      indent: '  ',
-      inlineCharacterLimit: Infinity,
-      transform: (obj, prop, originalResult) => {
-        const value = obj[prop]
-        return tersify(value, option)
-      }
-    })
-  str = str.replace(/\{/g, '{ ').replace(/\}/g, ' }').replace(/{\s{2}}/g, '{}').replace(/{\s{2}/g, '{ ').replace(/\s{2}}/g, ' }')
+    stringifyObject(obj, option)
   if (str.length > option.maxLength) {
     str = str.slice(0, option.maxLength - 5) + '...' + str.slice(-2)
   }
