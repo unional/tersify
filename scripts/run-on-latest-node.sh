b45e3181077353f10527d169d74ea2ddf934dbeb
Homa Wong <homawong@gmail.com>
2018-01-15 18:32:51 -0800

    chore: improve message

--- scripts/run-on-latest-node.sh
+++ scripts/run-on-latest-node.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 node_version=$(node -v);
 if [ ${node_version:1:1} = 9 ]; then
-  eval $1
+  eval $1;
+else
+  echo "NodeJS ${node_version} instead of latest (9) is detected. Skipping command";
 fi
