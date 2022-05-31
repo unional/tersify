51d54882be25ffc3b61c27100d609b287afed6a1
Homa Wong <homawong@gmail.com>
2022-05-31 00:43:57 -0700

    chore: webpack intirm

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,7 +1,9 @@
 'use strict';
 const paramCase = require('param-case').paramCase
 const pascalCase = require('pascal-case').pascalCase
-const path = require('path')
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const { NormalModuleReplacementPlugin } = require('webpack');
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 const pkg = require('./package.json')
@@ -12,22 +14,39 @@ const library = pascalCase(filename)
 module.exports = {
   mode: 'production',
   devtool: 'source-map',
-  entry: './ts/index.js',
+  entry: './ts/index',
   module: {
     rules: [
       {
         loader: 'ts-loader',
-        test: /\.tsx?$/,
+        test: /\.ts$/,
         options: {
           configFile: 'tsconfig.cjs.json',
           transpileOnly: true
         }
-      }
+      },
+      {
+        loader: 'babel-loader',
+        test: /\.js$/,
+        options:
+        {
+          presets: [
+            '@babel/preset-env',
+            '@babel/preset-typescript'
+          ],
+          plugins: ['@babel/plugin-transform-modules-commonjs']
+        }
+      },
+      // {
+      //   test: /\.[jt]s$/,
+      //   enforce: 'pre',
+      //   use: ['source-map-loader'],
+      // },
     ]
   },
-  // optimization: {
-  //   minimize: false
-  // },
+  optimization: {
+    minimize: true,
+  },
   // plugins: [
   //   new BundleAnalyzerPlugin()
   // ],
@@ -36,6 +55,12 @@ module.exports = {
     filename: `${filename}.es5.js`,
     library
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new NormalModuleReplacementPlugin(/.js$/, (resource) => {
+      resource.request = resource.request.replace(/.js$/, '')
+    })
+  ],
   resolve: {
     extensions: ['.ts', '.tsx', '.js'],
     mainFields: ['browser', 'main']
