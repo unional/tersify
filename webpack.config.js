f396f1f6d0cd7c6ab93d6d37c7f31632241512b2
Homa Wong <homawong@gmail.com>
2022-05-31 22:25:12 -0700

    chore: fix webpack setting

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 'use strict';
-const paramCase = require('param-case')
-const pascalCase = require('pascal-case')
+const paramCase = require('param-case').paramCase
+const pascalCase = require('pascal-case').pascalCase
 const path = require('path')
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
