59908a783c3a77be5f1db2e905d3cd9cd76285eb
unional <homawong@gmail.com>
2026-02-16 16:18:11 -0800

    refactor: migrate to monorepo structure with turbo

---
+++ commitlint.config.js
@@ -0,0 +1,9 @@
+export default {
+	extends: ['@commitlint/config-conventional'],
+	rules: {
+		'subject-case': [0],
+		'header-case': [0],
+		'header-max-length': [0],
+		'body-max-line-length': [0],
+	},
+}
