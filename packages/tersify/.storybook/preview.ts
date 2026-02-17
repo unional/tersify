dd72fee0d4256ea4917b3814ff017c96374db254
unional <homawong@gmail.com>
2026-02-16 22:05:17 -0800

    chore(config): refactor storybook preview configuration

--- packages/tersify/.storybook/preview.ts
+++ packages/tersify/.storybook/preview.ts
@@ -1,4 +1,3 @@
-import { defineParameters } from '@repobuddy/storybook'
 import type { Preview } from '@storybook/react-vite'
 import { defineDarkModeParam } from '@storybook-community/storybook-dark-mode'
 
@@ -6,27 +5,42 @@ import './tailwind.css'
 import './tailwind.repobuddy-storybook.css'
 
 const preview: Preview = {
-	parameters: defineParameters(
-		{
-			controls: {
-				matchers: {
-					color: /(background|color)$/i,
-					date: /Date$/i
-				}
-			},
-			a11y: {
-				test: 'todo'
-			},
-			docs: {
-				codePanel: true
+	parameters: {
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/i
 			}
 		},
-		defineDarkModeParam({
+		a11y: {
+			test: 'todo'
+		},
+		docs: {
+			codePanel: true
+		},
+		options: {
+			storySort: {
+				method: 'alphabetical',
+				order: [
+					'Overview',
+					'Changelog',
+					'components',
+					'decorators',
+					'parameters',
+					'arg-types',
+					'types',
+					'manager',
+					'testing',
+					'*'
+				]
+			}
+		},
+		...defineDarkModeParam({
 			classTarget: 'html',
 			stylePreview: true,
 			darkClass: ['dark', 'bg-black', 'text-white']
 		})
-	)
+	}
 }
 
 export default preview
